828cfb3f355bdb8f946c9745e5c278ace581c004
AHADKHAN2202 <ahad.khan7999782202@gmail.com>
2023-01-12 17:43:22 +0530

    added some more work

---
+++ L2.js
@@ -0,0 +1,30 @@
+// variables in javascript and data types
+
+// let a = 17;
+// console.log(a);
+// let , var and const  are used to declare variable 
+// a is a identifier or variable name
+// = is a assignment operator
+// 17 is literal or constant or assigning value to varible
+// and the whole process is known as declaring variable
+
+
+// let a = 17;
+// console.log(a);
+// a = "Harry Potter"; // we can change the variable type at run time that's why it is dynamically typed language.
+// console.log(a);
+
+// how to declare variable
+
+// let 123a = 4; // wrong declaration
+// let @#A = 4; // wrong declaration
+
+// let $name = "ahad"; // right declaration
+// let name_lastname = "khan"; // right declaration
+// let name123 = "ahad"; // right declaration
+
+
+// let ahad = "khan";
+// let Ahad = "khan" 
+// both will work because js is case sensitive it will take both the variables two variables. but we don't have to do this type of thing in making a program 
+// because it decreses readability of the program.
