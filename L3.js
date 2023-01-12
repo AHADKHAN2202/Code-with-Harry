4648695ecb0257f0754a5576a1754afaa2c70673
AHADKHAN2202 <ahad.khan7999782202@gmail.com>
2023-01-13 00:27:17 +0530

    complted 5 lectures

---
+++ L3.js
@@ -0,0 +1,47 @@
+// difference between let , var and const
+// var
+// var a = 30; 
+// var a ;
+// console.log(a);
+// // var is a global scope it can be accessed outside the scope
+// // we can re-declare the same varible with var and update the  same varaible 
+// we can ini
+
+// {
+//     var b = 60;
+//     var b = 90; 
+//     // console.log(b);
+// }
+
+// console.log(b);
+
+// let 
+
+// let a =80;
+// let a = 90;
+//  let a =;
+//  console.log(a);
+// let can be accesed outside the scope so it is block scope.
+// it cannot be re-declare the variable with let 
+// we can update the same variable.
+// {
+//     let a = 90;
+//     // console.log(a);
+// }
+// console.log(a);
+
+// const
+
+// const a = 90;
+// const a = 100;
+// console.log(a);
+//    const a;
+//    console.log(a);
+// const cannot be accesed outside the block scope
+// we cannot re-declare variable using const and we cannot update the same variable using const.
+// {
+//     const a = 40;
+//     a = 60;
+//     console.log(a);
+// }
+// console.log(a);
--- practiceSet1.js
+++ practiceSet1.js
@@ -4,3 +4,39 @@
 
 // console.log(variable+number);
 
+// question 2
+
+// console.log(typeof(variable+number));
+
+// question 3
+
+// const obj = {
+//     name : "Ahad",
+//     last_name: "khan",
+//     graduation:"be",
+//     cgpa:7.35,
+//     coder:true
+// }
+// if we use let and var variable we can change but in const we can't
+// obj = 123;
+
+// console.log(obj);
+
+// question 4
+
+// obj.working = false; // added new key
+// obj.name = "Rahul" // updated the value with key with name
+// console.log(obj)
+
+
+// question 5
+
+// const dict = {
+//     run : "daurna",
+//     write : "likhna",
+//     code : 'bhau parkat krna',
+//     play : "khelna",
+//     sing : "gun gunana"
+// }
+
+// console.log(dict.play);
